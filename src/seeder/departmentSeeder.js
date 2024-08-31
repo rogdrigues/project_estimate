f3327bb65a217267a9c2250fb4e252fd95ee41a4
phong <phongnhhe163436@fpt.edu.vn>
2024-08-31 21:23:40 +0700

    fix: fix error giving when create data fake for department

--- src/seeder/departmentSeeder.js
+++ src/seeder/departmentSeeder.js
@@ -4,53 +4,57 @@ const UserMaster = require('../models/userMaster');
 const Division = require('../models/division');
 
 const seedDepartments = async () => {
-    await Department.deleteMany();
-    const departmentLeadRole = await UserMaster.find()
-        .populate('role');
+    try {
+        await Department.deleteMany();
+        const departmentLeadRole = await UserMaster.find()
+            .populate('role');
 
-    const departmentLeads = departmentLeadRole.filter(user => user.role && user.role.roleName === 'Department Lead');
+        const departmentLeads = departmentLeadRole.filter(user => user.role && user.role.roleName === 'Department Lead');
 
-    const divisions = await Division.find();
+        const divisions = await Division.find();
 
-    const departments = [];
+        const departments = [];
 
-    divisions.forEach((division, index) => {
-        const lead = departmentLeads[index % departmentLeads.length];
+        divisions.forEach((division, index) => {
+            const lead = departmentLeads[index % departmentLeads.length];
 
-        departments.push(
-            {
-                code: `HRD-H${index + 1}`,
-                name: `HR Department ${index + 1}`,
-                division: division._id,
-                lead: lead._id,
-                description: `This is HR Department ${index + 1}`
-            },
-            {
-                code: `ITD-K${index + 1}`,
-                name: `IT Department ${index + 1}`,
-                division: division._id,
-                lead: lead._id,
-                description: `This is IT Department ${index + 1}`
-            },
-            {
-                code: `FND-Z${index + 1}`,
-                name: `Finance Department ${index + 1}`,
-                division: division._id,
-                lead: lead._id,
-                description: `This is Finance Department ${index + 1}`
-            }
-        );
-    });
+            departments.push(
+                {
+                    code: `HRD-H${index + 1}`,
+                    name: `HR Department ${index + 1}`,
+                    division: division._id,
+                    lead: lead._id,
+                    description: `This is HR Department ${index + 1}`
+                },
+                {
+                    code: `ITD-K${index + 1}`,
+                    name: `IT Department ${index + 1}`,
+                    division: division._id,
+                    lead: lead._id,
+                    description: `This is IT Department ${index + 1}`
+                },
+                {
+                    code: `FND-Z${index + 1}`,
+                    name: `Finance Department ${index + 1}`,
+                    division: division._id,
+                    lead: lead._id,
+                    description: `This is Finance Department ${index + 1}`
+                }
+            );
+        });
 
-    const createdDepartments = await Department.insertMany(departments);
-    console.log('Departments created successfully');
+        const createdDepartments = await Department.insertMany(departments);
+        console.log('Departments created successfully');
 
-    for (let i = 0; i < createdDepartments.length; i++) {
-        const lead = departmentLeads[i % departmentLeads.length];
-        lead.department = createdDepartments[i]._id;
-        await lead.save();
+        for (let i = 0; i < createdDepartments.length; i++) {
+            const lead = departmentLeads[i % departmentLeads.length];
+            lead.department = createdDepartments[i]._id;
+            await lead.save();
+        }
+        console.log('Department leads updated successfully');
+    } catch (err) {
+        console.error('Error while creating departments:', err.message);
     }
-    console.log('Department leads updated successfully');
 };
 
 module.exports = seedDepartments;
